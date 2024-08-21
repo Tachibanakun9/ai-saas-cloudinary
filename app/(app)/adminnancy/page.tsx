48c74d55046e784daba5aef167faef4643fb9a55
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-21 04:26:43 +0000

    admin page working succesfullf

--- app/(app)/adminnancy/page.tsx
+++ app/(app)/adminnancy/page.tsx
@@ -1,7 +1,23 @@
-'use client'
+import { redirect } from 'next/navigation'
+import { checkRole } from '@/utils/roles'
 
-function adminnancy() {
-  return <h1>okkk</h1>
+import { SearchUsers } from './_search-users'
+import { clerkClient } from '@clerk/nextjs/server'
+import { setRole } from './_actions'
+
+
+
+
+export default async function AdminDashboard() {
+  // If the user does not have the admin role, redirect them to the home page
+  if (!checkRole('admin')) {
+    redirect('/')
+  }
+
+  return (
+    <>
+      <h1>This is the admin dashboard</h1>
+      <p>This page is restricted to users with the 'admin' role.</p>
+    </>
+  )
 }
-
-export default adminnancy
