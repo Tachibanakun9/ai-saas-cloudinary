e32402b3ea29d4ae95c44dabf50e3165243d3dea
Tachibanakun9 <167691698+Tachibanakun9@users.noreply.github.com>
2024-08-25 11:04:16 +0530

    Update page.tsx
    export default function Home() {
      return <main>
        <div className = "text-center">hellow world</div>
      </main>
      
    }}. the code in app/(app)/adminnancy/page.tsx => { import { redirect } from 'next/navigation'
    import { checkRole } from '@/utils/roles'
    
    import { SearchUsers } from './_search-users'
    import { clerkClient } from '@clerk/nextjs/server'
    import { setRole } from './_actions'
    
    
    
    
    export default async function AdminDashboard() {
      // If the user does not have the admin role, redirect them to the home page
      if (!checkRole('admin')) {
        redirect('/')
      }
    
      return (
        <>
          <h1>This is the admin dashboard</h1>
          <p>This page is restricted to users with the admin role</p>
        </>
      )
    }

--- app/(app)/adminnancy/page.tsx
+++ app/(app)/adminnancy/page.tsx
@@ -1,12 +1,8 @@
 import { redirect } from 'next/navigation'
 import { checkRole } from '@/utils/roles'
-
 import { SearchUsers } from './_search-users'
 import { clerkClient } from '@clerk/nextjs/server'
 import { setRole } from './_actions'
-
-
-
 
 export default async function AdminDashboard() {
   // If the user does not have the admin role, redirect them to the home page
@@ -17,7 +13,7 @@ export default async function AdminDashboard() {
   return (
     <>
       <h1>This is the admin dashboard</h1>
-      <p>This page is restricted to users with the admin role</p>
+      <p>This page is restricted to users with the admin role.</p>
     </>
   )
 }
